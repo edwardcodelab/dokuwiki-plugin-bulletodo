da636aeec91450229e4a92870f48d1ba8817e688
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-11-01 14:23:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var STbullet = ['#d','#c','#l','#r','#t','#bt','#lt','#rt'];
+var SBbullet = ['⏺','○','⇦','⇨','△','▲','⏴','⏵'];
+jQuery('.todoinnertext').each(function(){
+        var rawtext=jQuery(this).text();
+		       console.log(rawtext);
+	for(var i = 0; i < STbullet.length; i+=1) {
+       if (jQuery(this).text().match(STbullet[i])){
+          var newtext= rawtext.replace(STbullet[i], SBbullet[i]);
+	      jQuery(this).text(newtext);
+	       console.log(newtext);
+          break;
+       }
+    }
+ });
+
+
