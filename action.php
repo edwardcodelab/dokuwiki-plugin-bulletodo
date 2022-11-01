5b71c7cb60f8c231bd96c3ec031638994cb9ec46
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-11-01 12:56:59 +0800

    Update action.php

--- action.php
+++ action.php
@@ -14,7 +14,7 @@ class action_plugin_bulletodo extends \dokuwiki\Extension\ActionPlugin
              $controller->register_hook('TPL_ACT_RENDER', 'BEFORE', $this, 'bulletlist');
  		}
              
-function bulletlist($event,$param) {
+function bulletlist(Doku_Event $event,$param) {
         $beforesymbol = $this->getConf('before_symbol');
         $aftersymbol = $this->getConf('after_symbol');
     echo '<p>'.$beforesymbol.$aftersymbol.'</p>';
