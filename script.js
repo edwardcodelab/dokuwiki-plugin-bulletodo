a00ae3a40f3367bddb747c6620eb8d51a8f27e32
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-11-01 13:59:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var STbullet = ['#d','#c','#l','#r','#t','#bt','#lt','#rt'];
 var SBbullet = ['⏺','○','⇦','⇨','△','▲','⏴','⏵'];
 jQuery('.todoinnertext').each(function(){
         var rawtext=jQuery(this).text();
+		       console.log(rawtext);
 	for(var i = 0; i < STbullet.length; i+=1) {
        if (jQuery(this).text().match(STbullet[i])){
           newtext= rawtext.replace(STbullet[i], SBbullet[i]);
 	      jQuery(this).text(newtext);
+	       console.log(newtext);
           break;
        }
     }
