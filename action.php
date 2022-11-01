e0cff8990a2a24e3ccbfa8e58e0fb030e6cba8c9
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-11-01 13:55:30 +0800

    Create action.php

---
+++ action.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * DokuWiki Plugin bulletodo (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  dodotori <dodotori@localhost>
+  
+   
+ */
+
+class action_plugin_bulletodo extends \dokuwiki\Extension\ActionPlugin
+{
+function register(Doku_Event_Handler $controller) {
+             $controller->register_hook('TPL_ACT_RENDER', 'AFTER', $this, 'bulletshow');
+ 		}
+ function bulletshow($event, $param) {
+   
+   
+ }
+  
+}
